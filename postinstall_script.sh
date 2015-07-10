c49945c1c61803ca7948d820279cd16aaaddd261
Lee Ramsay <lee@ii.net>
2015-07-10 10:45:29 +0800

    fixed up examples more

--- postinstall_script.sh
+++ postinstall_script.sh
@@ -4,5 +4,5 @@ PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki export PATH
 
 # Change stuff.
 
-
+#/usr/sbin/set_stuff -True
 
--- uninstall_script.sh
+++ uninstall_script.sh
@@ -4,5 +4,5 @@ PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki export PATH
 
 # Put stuff back before we changed stuff.
 
-
+#/usr/sbin/set_stuff -False
 
