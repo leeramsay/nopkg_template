28a670b07d31dd68321de812cb30a1e6f02707c0
Lee Ramsay <lee@ii.net>
2015-07-10 10:30:05 +0800

    added comments, changed perms

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#this is the build script for a nopkg template.
+
+#edit the variables below, and then run build.sh
+
+#variables
 version="1.0"
 name="template"
 displayname="fluffly template"
@@ -8,5 +13,6 @@ uninstallcheck_script="uninstallcheck_script.sh"
 postinstall_script="postinstall_script.sh"
 uninstall_script="uninstall_script.sh"
 
+#business time
 makepkginfo --nopkg --name="${name}" --displayname="${displayname}" --pkgvers="${pkgvers}" --installcheck_script="${installcheck_script}" --uninstallcheck_script="${uninstallcheck_script}" --postinstall_script="${postinstall_script}" --uninstall_script="${uninstall_script}"
 
