0d7b47dc27c4b48140ece922386479925eba1c83
Lee Ramsay <lee@ii.net>
2015-07-10 10:34:27 +0800

    added error checking and output to build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,13 @@ installcheck_script="installcheck_script.sh"
 uninstallcheck_script="uninstallcheck_script.sh"
 postinstall_script="postinstall_script.sh"
 uninstall_script="uninstall_script.sh"
+pkginfo_extension="plist" #you may use pkginfo?
 
 #business time
-makepkginfo --nopkg --name="${name}" --displayname="${displayname}" --pkgvers="${pkgvers}" --installcheck_script="${installcheck_script}" --uninstallcheck_script="${uninstallcheck_script}" --postinstall_script="${postinstall_script}" --uninstall_script="${uninstall_script}"
 
+#construct file path
+output="${name}-${version}.${pkginfo_extension}"
+
+makepkginfo --nopkg --name="${name}" --displayname="${displayname}" --pkgvers="${pkgvers}" --installcheck_script="${installcheck_script}" --uninstallcheck_script="${uninstallcheck_script}" --postinstall_script="${postinstall_script}" --uninstall_script="${uninstall_script}" > "${output}"
+
+[[ $? == 0 ]] && { echo "Successfully built pkginfo in working directory, named: ${output}"; } || { rm "${output}"; echo "Could not succesfully create pkginfo file"; }
